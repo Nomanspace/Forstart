a99fe7bbe5021ee4386f0e1efb240ee363d1befb
Nomanspace <Warhammerd@gmail.com>
2021-10-14 18:13:14 +0300

    Попробовал тернарный оператор-не вышло. Попробовал через условие "иф елс"-вышло.

--- src/Practice3.java
+++ src/Practice3.java
@@ -1,5 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Practice3 {
     public static void main(String[] args) {
+//Создаем экземпляр класса Random()
+        Random rand = new Random();
+        Scanner scanner = new Scanner(System.in);
 
+        String win = "you win";
+        String loose = "you loose";
+
+        //Создаем переменную типа int
+        int randInt;
+
+        //Сохраняем случайное число в переменной randInt;
+        randInt = rand.nextInt(10) + 1;
+        System.out.println("кожанный мешок, я загадал " + randInt);
+
+        //создаем пользовательскую переменную
+        System.out.println("введите своё число");
+        int userenter = scanner.nextInt();
+
+        //userenter == randInt ? System.out.println(win) : System.out.println(loose);
+
+        if (userenter == randInt) {
+            System.out.println(win);
+        } else {
+            System.out.println(loose);
+        }
     }
 }
