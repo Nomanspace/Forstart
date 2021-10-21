eaf2bbd774d8fa9bd18ef8adc678c4e5620dcc6a
Nomanspace <Warhammerd@gmail.com>
2021-10-21 17:54:12 +0300

    Не получается вызвать метод.

--- src/Practice7.java
+++ src/Practice7.java
@@ -9,33 +9,59 @@ import java.util.Scanner;
 
 public class Practice7 {
     public static void main(String[] args) {
-        // Perimetr triangle =
-        //Perimetr square =
-        // Perimetr circle =
-    }
-
-    static class Triangle {
-        Scanner scanpT = new Scanner(System.in);
-        int length;
-        int pT = length * 3;
-
-    }
-
-    static class Square {
-        Scanner scanpS = new Scanner(System.in);
-        int a;
-        int pS = a * 4;
-    }
-
-    static class Circle {
-        double PI = 3.14159;
-
-        static double perimetrCicrle(double PI, int radius) {
-            Scanner scanpC = new Scanner(System.in);
-            System.out.println("Введите " + radius);
-            radius = scanpC.nextInt();
-            double pC = 2 * PI * radius;
-            return pC;
+        Scanner scanner = new Scanner(System.in);
+        //triangle
+        //square
+        //circle
+        char triangle = scanner.next().charAt(0);
+        //char b = 'b';
+        if (triangle == 'a') {
+            perimetrTriangle();
+        } else {
+            System.out.println("что-то пошло не так");
         }
     }
+
+    //static class Triangle {
+
+    static int perimetrTriangle(int length, int pT) {
+        /*static*/
+        //int length;
+        //length = length1;
+        Scanner scanpT = new Scanner(System.in);
+        System.out.println("введите length ");
+        length = scanpT.nextInt();
+        return pT = length * 3;
+        //return pT;
+    }
+    //  }
+
+    //static class Square {
+
+    static int perimetrSqare(int a) {
+        /*static*/
+        //int a;
+        // a = a1;
+        Scanner scanpS = new Scanner(System.in);
+        a = scanpS.nextInt();
+        System.out.println("Введите a " + a);
+        int pS = a * 4;
+        return pS;
+    }
+    //  }
+
+    // static class Circle {
+
+    static double perimetrCicrle(/*double PI*/ int radius) {
+        /*static*/
+        double PI = 3.14159;
+        //PI = PI1;
+        Scanner scanpC = new Scanner(System.in);
+        System.out.println("Введите " + radius);
+        radius = scanpC.nextInt();
+        double pC = 2 * PI * radius;
+        return pC;
+    }
+    //}
 }
+
