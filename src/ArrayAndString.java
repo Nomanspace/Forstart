bce0cb3dc09b2000e63f31477f641bb3e43061c4
Nomanspace <Warhammerd@gmail.com>
2021-10-22 19:06:12 +0300

    Измените код из предыдущего задания так, чтобы вся проверка имени была выделена в отдельный метод с именем, например, checkUserName().

--- src/ArrayAndString.java
+++ src/ArrayAndString.java
@@ -1,3 +1,10 @@
+/**
+ * Измените код из предыдущего задания так, чтобы в случае совпадения имен пользователей (без учета регистра) выводился ответ:
+ * "Выберите другое имя пользователя", а если имена не совпадают, выводится ответ: "Отличное имя!".
+ * В задании используйте методы из таблицы выше.
+ */
+
+
 public class ArrayAndString {
     public static void main(String[] args) {
         //String a = "Sometimes";
@@ -5,7 +12,15 @@ public class ArrayAndString {
         String a1 = "Иван Иванов";
         String a2 = "иван иванов";
         //System.out.println(a1.equals(a2));
-        System.out.println(a1.equalsIgnoreCase(a2));
+        //System.out.println(a1.equalsIgnoreCase(a2));
+
+        if (a1.equalsIgnoreCase(a2)) {
+            System.out.println("Выберите другое имя");
+        } else if (!a1.equalsIgnoreCase(a2)) {
+            System.out.println("Отличное Имя");
+        } else {
+            System.out.println("somethings get wrong");
+        }
 
     }
 }
