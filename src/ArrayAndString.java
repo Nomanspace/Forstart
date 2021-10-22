a68341e4cc6941dbae9fb0f9d3c2e83dce88c248
Nomanspace <Warhammerd@gmail.com>
2021-10-22 16:45:50 +0300

    Можно ли пользователю выбрать себе имя "Иван Иванов", если уже есть другой пользователь с именем "иван иванов", считая, что регистр НЕ важен для вашей системы?
    Напишите код, который сравнивает эти имена и выводит результат сравнения. В задании используйте методы из таблицы выше.

--- src/ArrayAndString.java
+++ src/ArrayAndString.java
@@ -1,6 +1,9 @@
 public class ArrayAndString {
     public static void main(String[] args) {
-        String a = "Sometimes";
-        System.out.println(a.charAt(2));
+        //String a = "Sometimes";
+        //System.out.println(a.charAt(2));
+        String a1 = "Иван Иванов";
+        String a2 = "иван иванов";
+        System.out.println(a1.equals(a2));
     }
 }
