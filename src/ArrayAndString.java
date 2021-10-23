be04cab9c8e269ae462a6a39ef55f415ab93b5b4
Nomanspace <Warhammerd@gmail.com>
2021-10-23 12:44:37 +0300

    Измените код из предыдущего задания так, чтобы после надписи о длине имени добавлялось замечание "А без пробелов ваше имя занимает <число> символов". Организуйте код таким образом, чтобы даже если имя состоит из трех слов, программа работала корректно. В задании используйте методы из таблицы выше.

--- src/ArrayAndString.java
+++ src/ArrayAndString.java
@@ -4,13 +4,19 @@
  * В задании используйте методы из таблицы выше.
  */
 
+import java.util.Scanner;
 
 public class ArrayAndString {
     public static void main(String[] args) {
+        Scanner scanuser1 = new Scanner(System.in);
+        Scanner scanuser2 = new Scanner(System.in);
         //String a = "Sometimes";
         //System.out.println(a.charAt(2));
-        String user1 = "Иван Иванов";
-        String user2 = "иван иванов";
+        System.out.println("Введите имя1");
+        String user1 = scanuser1.nextLine();
+        System.out.println("Введите имя2");
+        String user2 = scanuser2.nextLine();
+        System.out.println("Ваши имена " + user1 + " " + user2);
         //System.out.println(a1.equals(user2));
         //System.out.println(a1.equalsIgnoreCase(user2));
         checkUserName(user1, user2);
@@ -19,11 +25,12 @@ public class ArrayAndString {
     private static void checkUserName(String user1, String user2) {
 
         if (user1.equalsIgnoreCase(user2)) {
-            System.out.println("Выберите другое имя");
+            System.out.println("Выберите другое имя.");
         } else if (!user1.equalsIgnoreCase(user2)) {
-            System.out.println("Отличное Имя");
+            System.out.println("Ваше имя уникально.");
+            System.out.println("Ваше имя пользователя " + user1 + " уникальное! Ваше имя имеет длину " + user1.length() + " символов.");
         } else {
-            System.out.println("somethings get wrong");
+            System.out.println("somethings get wrong.");
         }
     }
 
