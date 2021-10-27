734a0d1494b604adf0a687644b696672684ea6f0
Nomanspace <Warhammerd@gmail.com>
2021-10-27 22:08:57 +0300

    Пользуясь таблицей методов для изменения строк, представленной выше в этом юните, реализуйте метод, который будет удалять из строки все пробелы, но уже с использованием новых методов.
    В этом задании нельзя использовать посимвольный обход слова.

---
+++ src/ArrayAndString2.java
@@ -0,0 +1,12 @@
+public class ArrayAndString2 {
+
+    public static void main(String[] args) {
+        System.out.println(removeWhiteSpaces("А роза упала на лапу Азора"));
+    }
+
+    private static String removeWhiteSpaces(String str) {
+        //Написать код здесь
+        return str.replaceAll(" ", "");
+    }
+}
+
